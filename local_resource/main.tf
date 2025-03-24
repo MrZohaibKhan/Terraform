1ab042efd80f717bb18bfc1508384efe1e2b4817
mrzohaibkhan <zohaibyousafzai15@gmail.com>
2025-03-24 00:52:14 +0000

    Implement resource type local and random successfully

---
+++ local_resource/main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "games" {
+    filename = "/home/light/games.txt"
+    content = "I love games but I don't play them"
+   }
---
+++ random_pet/main.tf
@@ -0,0 +1,9 @@
+resource "local_file" "games" {
+  filename = "/home/light/terraform/random_pet/games.txt"
+  content = "I love GTA5"
+  }
+resource "random_pet" "pet" {
+    prefix = "Mr"
+    separator = "."
+    length = "2"
+}
